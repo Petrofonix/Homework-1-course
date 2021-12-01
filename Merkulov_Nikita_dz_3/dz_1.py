5e3c4a6cd5ff297545da107285b3bada45b3cf05
Petrofonix <nkmerkul@gmail.com>
2021-12-01 11:20:27 +0300

    Домашняя работа выполнена

---
+++ Merkulov_Nikita_dz_3/dz_1.py
@@ -0,0 +1,19 @@
+
+
+def num_translate(word):
+    num_dict = {
+        'zero': 'ноль',
+        'one': 'один',
+        'two': 'два',
+        'three': 'три',
+        'four': 'четыре',
+        'five': 'пять',
+        'six': 'шесть',
+        'seven': 'семь',
+        'eight': 'восемь',
+        'nine': 'девять',
+        'ten': 'десять'}
+    return num_dict.get(word)
+
+
+print(num_translate(input('Введите число на английском: ')))
